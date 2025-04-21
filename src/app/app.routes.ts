5a87920062ae8a677eb4733852e9e2f50943d39b
thiarap <polancothiara02@gmail.com>
2025-04-21 11:03:43 -0300

    proyecto nuevo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { ProductoComponent } from './pagina/producto/producto.component';
+import { CarritoComponent } from './pagina/carrito/carrito.component';
+import { NosotrosComponent } from './pagina/nosotros/nosotros.component';
+import { OfertasComponent } from './pagina/ofertas/ofertas.component';
+import { ContactoComponent } from './pagina/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'inicio', component: InicioComponent },
+    { path: 'producto', component: ProductoComponent },
+    { path: 'carrito', component: CarritoComponent },
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'ofertas', component: OfertasComponent },
+    { path: 'contacto', component: ContactoComponent }
+];
