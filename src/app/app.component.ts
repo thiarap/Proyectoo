d2dc2b091227291b443cd2f9584c232ba6566192
thiarap <polancothiara02@gmail.com>
2025-04-22 11:05:04 -0300

    creacion de las cartas en productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,5 @@ import { FooterComponent } from './shared/footer/footer.component';
 })
 export class AppComponent {
   title = 'Proyecto';
+  
 }
--- src/app/pagina/producto/producto.component.ts
+++ src/app/pagina/producto/producto.component.ts
@@ -1,11 +1,29 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-producto',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.css'
 })
 export class ProductoComponent {
-
+  productos=[
+    {
+      id: 1,
+      img:"https://d3cdlnm7te7ky2.cloudfront.net/media/catalog/product/cache/1f1b8d9a1b49678555bea203c2f52e39/1/0/104340_3.jpg",
+      nombre: "producto1", 
+      precio: 100,
+    },
+    {
+      id: 2,
+      img:"",
+      nombre: "producto2", 
+      precio: 150,
+    },
+    {
+      nombre: "producto3", 
+      precio: 150,
+    },
+   ]; 
 }
