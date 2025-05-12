44078c0c8b2d4804038eb78510b7e28eabc8625d
thiarap <polancothiara02@gmail.com>
2025-05-12 11:06:50 -0300

    cambio essentials

--- src/app/pagina/producto/producto.component.ts
+++ src/app/pagina/producto/producto.component.ts
@@ -11,28 +11,28 @@ export class ProductoComponent {
   productos = [
     {
       id: 1,
-      img: "https://juleriaque.vtexassets.com/arquivos/ids/228689-800-auto?v=638488966737500000&width=800&height=auto&aspect=true",
+      img: "https://fearofgod.com/cdn/shop/files/LOOK_54_ETERNAL_SS25_LB_024_V1_900x.jpg?v=1733879882",
       marca: "RABANNE",
       nombre: "Phantom Intense EDP 100 ml",
       precio: 229.100,
     },
     {
       id: 2,
-      img: "https://juleriaque.vtexassets.com/arquivos/ids/249611-800-auto?v=638748979287600000&width=800&height=auto&aspect=true",
+      img: "",
       marca: "RABANNE",
       nombre: "Phantom Elixir Parfum Intense 100 ml",
       precio: 210.000,
     },
     {
       id: 3,
-      img:"https://juleriaque.vtexassets.com/arquivos/ids/202787-800-auto?v=638768655282970000&width=800&height=auto&aspect=true",
+      img:"",
       nombre: "Fame The Couture EDP 80 ml",
       marca: "RABANNE",
       precio: 290.100,
     },
     {
       id: 4,
-      img:"https://juleriaque.vtexassets.com/arquivos/ids/254383-800-auto?v=638808627339630000&width=800&height=auto&aspect=true",
+      img:"",
       nombre: "Phantom Parfum EDP 80 ml",
       marca: "RABANNE",
       precio: 233.100,
@@ -40,14 +40,14 @@ export class ProductoComponent {
     
     {
       id: 4,
-      img:"https://juleriaque.vtexassets.com/arquivos/ids/249618-800-auto?v=638748979352070000&width=800&height=auto&aspect=true",
+      img:"",
       nombre: "Fame The Couture EDP 80 ml",
       marca: "RABANNE",
       precio: 290.100,
     },
     {
       id: 5,
-      img: "https://juleriaque.vtexassets.com/arquivos/ids/226885-800-auto?v=638461157472900000&width=800&height=auto&aspect=true",
+      img: "",
       marca: "RABANNE",
       nombre: "Fame Parfum 80 ml Refillable",
       precio: 283.600,
@@ -55,21 +55,21 @@ export class ProductoComponent {
 
     {
       id: 6,
-      img: "https://juleriaque.vtexassets.com/arquivos/ids/234235-800-auto?v=638560640733900000&width=800&height=auto&aspect=true",
+      img: "",
       marca: "RABANNE",
       nombre: "Fame Intense EDP 80 ml Refillable",
       precio: 278.400,
     },
     {
       id: 7,
-      img: "https://juleriaque.vtexassets.com/arquivos/ids/227383-800-auto?v=638469739279030000&width=800&height=auto&aspect=true",
+      img: "",
       marca: "RABANNE",
       nombre: "Fame EDP 30 ml",
       precio: 265.500,
     },
     {
       id: 8,
-      img: "https://juleriaque.vtexassets.com/arquivos/ids/209425-800-auto?v=638149245461030000&width=800&height=auto&aspect=true",
+      img: "",
       marca: "RABANNE",
       nombre: "Fame EDP 80 ml Refillable",
       precio: 257.800,
