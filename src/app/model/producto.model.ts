14ee5e2667443962b39eb970ce6181b1c1522d7c
thiarap <polancothiara02@gmail.com>
2025-05-22 10:52:42 -0300

    empezando carrito

---
+++ src/app/model/producto.model.ts
@@ -0,0 +1,9 @@
+export interface Producto{
+    id: number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    imagen: string;
+    disponible: boolean;
+    cantidad?: number;
+}
